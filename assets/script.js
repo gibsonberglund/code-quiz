0313829e8c2e33bee03a036657fa8f31f1215e6b
Gibson Berglund <berglundgibson@gmail.com>
2022-11-15 16:34:59 -0600

    added start button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,31 @@
-var startQuiz = document.querySelector('.button');
+var startQuiz = document.querySelector('.startbutton');
+var intro = document.querySelector('#intro');
+var firstQ = document.querySelector('#firstQ');
+var qButtons = document.querySelector('#choices')
+var option1 = document.querySelector('#option1');
+var option2 = document.querySelector('#option2');
+var option3 = document.querySelector('#option3');
+var option4 = document.querySelector('#option4');
 
-startQuiz.addEventListener('click', function () {
+startQuiz.addEventListener('click', function (event) {
+    event.preventDefault();
     console.log("did I do this right?");
+    firstQ.textContent = "This is the first question";
+    intro.textContent = "";
+    option1.setAttribute("class", "startbutton");
+    option1.textContent = "option1";
+
+
 })
 
+
+//NOT DONE
+
+
+
+//display a row of buttons beneath the first q
+
+
+//DONE
 //set <p> attr to empty
-//display an <h3>
-//display a row of buttons beneath it
+//display the first question
