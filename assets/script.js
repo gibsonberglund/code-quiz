42cb8b6f2b66ba493c4c1686ac76cc76020799af
Gibson Berglund <berglundgibson@gmail.com>
2022-11-15 15:31:39 -0600

    initial javascript

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var startQuiz = document.querySelector('.button');
+
+startQuiz.addEventListener('click', function () {
+    console.log("did I do this right?");
+})
+
+//set <p> attr to empty
+//display an <h3>
+//display a row of buttons beneath it
