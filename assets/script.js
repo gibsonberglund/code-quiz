cd54dfceecb4ba0f2df9d1bd024aa8bc9a0dc54a
Gibson Berglund <berglundgibson@gmail.com>
2022-11-16 23:02:50 -0600

    changed the method of comparing useranswer to correct answer, and changed score incrementing

--- assets/script.js
+++ assets/script.js
@@ -37,12 +37,13 @@ function timerstart() {
     }, 1000);
   }
 
-
-function scoreUp(event) {
-    event.preventDefault();
+ /* 
+//increments score for correct answers
+function scoreUp() {
     score++;
     console.log(score);
 }
+*/
 
 //when start button is clicked, intro paragraph and start button disappear
 startQuiz.addEventListener('click', function (event) {
@@ -63,11 +64,22 @@ startQuiz.addEventListener('click', function (event) {
     option4.textContent = "BLOG";
 
 
-//listens for correct answer and augments score
+/*listens for correct answer and augments score
     option3.addEventListener('click', scoreUp)
+    */
+    var answ1 = "option3";
+
 //when an answer is clicked, move on to second question
     qButtons.addEventListener('click', function (event) {
-    event.preventDefault();
+    event.preventDefault(event);
+    var useransw1 = event.target.id;
+    if (useransw1 === answ1) {
+        score++;
+        console.log(score);
+        intro.textContent = "Correct!";
+    } else {
+        intro.textContent = "Incorrect :("
+    }
     secondQ();
     })
 
@@ -80,16 +92,20 @@ function secondQ () {
     option2.textContent = "Strings only";
     option3.textContent = "Numbers only";
     option4.textContent = "Numbers and strings only";
-    if (score===0) {
+
+    var answ2 = "option2";
+
+//listens for a click and moves on to question three
+    qButtons.addEventListener('click', function (event) {
+        event.preventDefault();
+        var useransw2 = event.target.id;
+    if (useransw2 === answ2) {
+        score++;
+        console.log(score);
         intro.textContent = "Correct!";
     } else {
         intro.textContent = "Incorrect :("
     }
-//listens for correct answer and augments score
-    option2.addEventListener('click', scoreUp)
-//listens for a click and moves on to question three
-    qButtons.addEventListener('click', function (event) {
-        event.preventDefault();
         thirdQ();
     })
 }
@@ -101,27 +117,45 @@ function thirdQ () {
     option2.textContent = "x.document.$";
     option3.textContent = "document.$('x')";
     option4.textContent = "$('x')";
-//listens for correct answer and augments score
-    option4.addEventListener('click', scoreUp);
+
+    var answ3 = "option4";
+
 //listens for a click and moves on to question three
     qButtons.addEventListener('click', function (event) {
         event.preventDefault();
+        var useransw3 = event.target.id;
+        if (useransw3 === answ3) {
+            score++;
+            console.log(score);
+            intro.textContent = "Correct!";
+        } else {
+        intro.textContent = "Incorrect :("
+        }
         fourthQ();
     })
 }
 
 //resets for fourth question
 function fourthQ () {
-    firstQ.textContent = "What does .stopPropagation do?"
+    firstQ.textContent = "What does .stopPropagation do?";
     option1.textContent = "Prevents the current function from bubbling up to the next parent function";
     option2.textContent = "Prevents parent functions from accessing the current function";
     option3.textContent = "Prevents the current function from executing more that once";
     option4.textContent = "Stops the current function from executing";
-    //listens for correct answer and augments score
-    option1.addEventListener('click', scoreUp);
+
+    var answ4 = "option1";
+
     //listens for a click and moves on to question three
     qButtons.addEventListener('click', function (event) {
         event.preventDefault();
+        var useransw4 = event.target.id;
+        if (useransw4 === answ4) {
+            score++;
+            console.log(score);
+            intro.textContent = "Correct!";
+        } else {
+        intro.textContent = "Incorrect :(";
+        }
         fifthQ();
     })
 }
@@ -133,11 +167,21 @@ function fifthQ () {
     option2.textContent = "scope";
     option3.textContent = "array";
     option4.textContent = "balloon";
-    //listens for correct answer and augments score
-    option2.addEventListener('click', scoreUp);
+
+    var answ5 = "option2";
+
     //listens for a click and moves on to question three
     qButtons.addEventListener('click', function (event) {
         event.preventDefault();
+        console.log(event.target.id);
+        var useransw5 = event.target.id;
+        if (useransw5 === answ5) {
+            score++;
+            console.log(score);
+            intro.textContent = "Correct!";
+        } else {
+        intro.textContent = "Incorrect :(";
+        }
         sixthQ();
     })
 }
@@ -149,14 +193,24 @@ function sixthQ () {
     option2.textContent = ".textContent";
     option3.textContent = "console.log";
     option4.textContent = ".setAttribute";
-    //listens for correct answer and augments score
-    option4.addEventListener('click', scoreUp);
+
+    var answ6 = "option4";
+
     //listens for a click and moves on to question three
     qButtons.addEventListener('click', function (event) {
         event.preventDefault();
+        var useransw6 = event.target.id;
+        if (useransw6 === answ6) {
+            score++;
+            console.log(score);
+            intro.textContent = "Correct!";
+        } else {
+        intro.textContent = "Incorrect :("
+        }
         yourScore();
     })
 }
+
 var hiScore = document.querySelector('.hiScore');
 var submitBtn = document.querySelector(".submitBtn");
 
@@ -164,7 +218,7 @@ var submitBtn = document.querySelector(".submitBtn");
 function yourScore () {
     firstQ.textContent = "Here is your score:"
     qButtons.style.display = "none";
-    scoredisplay.textContent = score;
+    scoredisplay.textContent = "Your score: " + score;
     var hiScoreInfo = {
         username: hiScore.value,
         userscore: score,
@@ -182,6 +236,7 @@ NOT DONE
 tally correct and incorrect answers
 add a high scores form to final screen
 hide high scores textfield and submit button until final screen
+wrong answers decrement time-remaining
 
 DONE
 add timer
